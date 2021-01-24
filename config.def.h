f6393bdf8f64dd87c2611f152f42e606299a97e0
gomfol12 <marek@am-hohenstein.de>
2021-01-24 15:33:02 +0100

    remove hack added nerd-font-iosevka patch

--- config.def.h
+++ config.def.h
@@ -5,10 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Medium Extended:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Iosevka Regular:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true",
-						 "Hack Nerd Font Regular:pixelsize=12:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 
 static int borderpx = 1;
 
