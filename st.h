170cc45582b32218979d9967b8a9450755a39119
gomfol12 <marek@am-hohenstein.de>
2021-04-19 13:43:34 +0200

    added patch openclipboard

--- st.h
+++ st.h
@@ -90,6 +90,7 @@ void printsel(const Arg *);
 void sendbreak(const Arg *);
 void toggleprinter(const Arg *);
 void copyurl(const Arg *);
+void opencopied(const Arg *);
 
 int tattrset(int);
 int tisaltscr(void);
