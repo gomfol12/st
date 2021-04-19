0d84a1513110f8505425b82778c35100bac4d4ef
gomfol12 <marek@am-hohenstein.de>
2021-04-19 14:06:33 +0200

    added patch xresources

--- config.def.h
+++ config.def.h
@@ -137,7 +137,11 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#666666"
+    "#ffffff",
+	"#000000",
+	"#666666",
+    "#ffffff",
+    "#000000",
 };
 
 
@@ -145,14 +149,14 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 7;
-static unsigned int defaultrcs = 7;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+static unsigned int defaultcs = 260;
+static unsigned int defaultrcs = 260;
 
 /* Colors used for selection */
-unsigned int selectionbg = 256;
-unsigned int selectionfg = 7;
+unsigned int selectionbg = 258;
+unsigned int selectionfg = 259;
 /* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
 /* Else if 1 keep original foreground-color of each cell => more colors :) */
 static int ignoreselfg = 1;
@@ -199,6 +203,33 @@ static unsigned int defaultattr = 11;
  * modifier, set to 0 to not use it.
  */
 static uint forcemousemod = ShiftMask;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[0] },
+		{ "color1",       STRING,  &colorname[1] },
+		{ "color2",       STRING,  &colorname[2] },
+		{ "color3",       STRING,  &colorname[3] },
+		{ "color4",       STRING,  &colorname[4] },
+		{ "color5",       STRING,  &colorname[5] },
+		{ "color6",       STRING,  &colorname[6] },
+		{ "color7",       STRING,  &colorname[7] },
+		{ "color8",       STRING,  &colorname[8] },
+		{ "color9",       STRING,  &colorname[9] },
+		{ "color10",      STRING,  &colorname[10] },
+		{ "color11",      STRING,  &colorname[11] },
+		{ "color12",      STRING,  &colorname[12] },
+		{ "color13",      STRING,  &colorname[13] },
+		{ "color14",      STRING,  &colorname[14] },
+		{ "color15",      STRING,  &colorname[15] },
+		{ "foreground",   STRING,  &colorname[256] },
+		{ "background",   STRING,  &colorname[257] },
+		{ "selectionbg",  STRING,  &colorname[258] },
+		{ "selectionfg",  STRING,  &colorname[259] },
+		{ "cursorColor",  STRING,  &colorname[260] },
+};
 
 /*
  * Internal mouse shortcuts.
